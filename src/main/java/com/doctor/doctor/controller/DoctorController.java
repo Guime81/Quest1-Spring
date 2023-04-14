e91242c56c5dc38e75c89d9ada169c7f96b7b7f8
Guime81 <guillaume.devup@gmail.com>
2023-04-14 16:12:20 +0200

    challenge finish

---
+++ src/main/java/com/doctor/doctor/controller/DoctorController.java
@@ -0,0 +1,27 @@
+package com.doctor.doctor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/1")
+    @ResponseBody
+    public String getDoctor1() {
+        return "William Hartnell ✌️";
+    }
+
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String getDoctor10() {
+        return "David Tennant 🆗";
+    }
+
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String getDoctor13() {
+        return "Jodie Whittaker 😎🎶";
+    }
+}
